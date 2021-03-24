854c96b3652becd0a232a9a40619023f75c28d7e
unknown <milan_cirkovic@yahoo.com>
2021-03-24 23:39:52 +0100

    Makeing load more option

---
+++ src/components/load-more/loadMore.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Button from "../forms/button/button";
+
+function LoadMore({ onLoadMoreEvent }) {
+  return (
+    <div>
+      <Button>Load More</Button>
+    </div>
+  );
+}
+
+export default LoadMore;
