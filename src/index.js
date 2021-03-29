a7536c6820311ed975f7cd59b49dc736379f5570
unknown <milan_cirkovic@yahoo.com>
2021-03-29 22:42:39 +0200

    implemented redux-persist to the store, cart store

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./redux/createStore";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./redux/createStore";
 
 import App from "./App";
 
@@ -10,7 +11,9 @@ ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <Router>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </Router>
     </Provider>
   </React.StrictMode>,
--- src/redux/createStore.js
+++ src/redux/createStore.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
+import { persistStore } from "redux-persist";
 
 import createSagaMiddleware from "redux-saga";
 
@@ -14,4 +15,6 @@ export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 sagaMiddleware.run(rootSaga); //must come after the store !!!!!
 
-export default store;
+export const persistor = persistStore(store);
+
+export default { store, persistor };
