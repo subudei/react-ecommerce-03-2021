776f782db60e0590ce2378278c7736407a591dda
unknown <milan_cirkovic@yahoo.com>
2021-03-30 19:16:41 +0200

    created firebase function & deployed it

---
+++ functions/index.js
@@ -0,0 +1,27 @@
+const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(
+  cors({
+    origin: true,
+  })
+);
+
+app.use(express.json());
+
+app.get("*", (req, res) => {
+  res.status(404).send("404, Not Found. ");
+});
+
+exports.api = functions.https.onRequest(app);
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
